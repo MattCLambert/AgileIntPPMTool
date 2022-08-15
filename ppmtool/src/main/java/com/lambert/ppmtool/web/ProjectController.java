85ce8e39215ef4ceb0bf2d58e4dda456ebd5953e
MattCLambert <matthew@mylambert.com>
2022-08-15 16:05:40 -0500

    Added the ability to search for an project by its ID

--- ppmtool/src/main/java/com/lambert/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/lambert/ppmtool/web/ProjectController.java
@@ -1,15 +1,13 @@
 package com.lambert.ppmtool.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,13 +24,24 @@ public class ProjectController {
 	private ProjectService projectService;
 	@Autowired
 	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,BindingResult result)
 	{
 		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-		if(errorMap != null) return errorMap;
+		if(errorMap != null) 
+		{
+			return errorMap;
+		};
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
+		
 	}
 	
+	@GetMapping("/{projectId}")
+	public ResponseEntity<?> getProjectById(@PathVariable String projectId)
+	{
+		Project project = projectService.findProjectByIdentifier(projectId);
+		return new ResponseEntity<Project>(project,HttpStatus.OK);
+	}
 }
